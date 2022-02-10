54ecdbd065baa2d0e8b6fcb9a2bacccefee75dc9
Robert C. Duvall <rcd@cs.duke.edu>
2022-02-10 17:06:03 -0500

    Added initial project files

---
+++ src/main/java/slogo/Main.java
@@ -0,0 +1,68 @@
+package slogo;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+/**
+ * Feel free to completely change this code or delete it entirely. 
+ */
+public class Main {
+    private static final String LANGUAGE_RESOURCE_PATH = "/slogo/languages/";
+    private static final String EXAMPLE_PROGRAMS_PATH = "/examples";
+
+    /**
+     * Get command in a given language.
+     */
+    public String getCommand (String language, String command) {
+        ResourceBundle resources = ResourceBundle.getBundle(LANGUAGE_RESOURCE_PATH + language);
+        return resources.getString(command);
+    }
+
+    /**
+     * Get first line of given example program.
+     */
+    public String getExampleProgram (String category, String example) {
+        // regular expression representing one or more whitespace characters (space, tab, or newline)
+        final String NEWLINE = "\\n+";
+
+        List<String> lines = readFile(String.format("%s/%s/%s.slogo", EXAMPLE_PROGRAMS_PATH, category, example), NEWLINE);
+        return lines.get(0).trim();
+    }
+
+    /**
+     * A method to test (and a joke :).
+     */
+    public double getVersion () {
+        return 0.001;
+    }
+
+    // this code is dense, hard to read, and throws exceptions so better to wrap in method
+    private List<String> readFile (String filename, String delimiter) {
+        try {
+            String path = getClass().getResource(filename).toExternalForm();
+            String data = new String(Files.readAllBytes(Paths.get(new URI(path))));
+            return Arrays.asList(data.split(delimiter));
+        }
+        catch (URISyntaxException | IOException | NullPointerException e) {
+            // NOT ideal way to handle exception, but this is just a simple test program
+            System.out.println("ERROR: Unable to read input file " + e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+
+
+    /**
+     * Start of the program.
+     */
+    public static void main (String[] args) {
+        Main m = new Main();
+        System.out.println(m.getVersion());
+        System.out.println(m.getCommand("English", "Forward"));
+        System.out.println(m.getExampleProgram("loops", "star"));
+    }
+}
---
+++ src/test/java/slogo/MainTest.java
@@ -0,0 +1,46 @@
+package slogo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Feel free to completely change this code or delete it entirely. 
+ */
+class MainTest {
+    private Main m;
+
+    // create new instance of test object before each test is run
+    @BeforeEach
+    void setup () {
+        m = new Main();
+    }
+
+
+    @Test
+    void testVersionIsReady () {
+        // how close do real valued numbers need to be to count as the same
+        final double TOLERANCE = 0.0005;
+
+        // different ways to test double results
+        assertEquals(1, Math.round(m.getVersion() * 1000));
+        assertTrue(m.getVersion() < 1);
+        assertEquals(0.001, m.getVersion(), TOLERANCE);
+    }
+
+    @Test
+    void testReadExampleProgram () {
+        assertEquals("fd sum 1 random 100", m.getExampleProgram("simple", "random_fun"));
+        assertEquals("to triangle [ ]", m.getExampleProgram("procedures", "geometry"));
+    }
+
+    @Test
+    void testReadLanguageProperties () {
+        final String COMMAND = "Forward";
+
+        assertEquals("forward|fd", m.getCommand("English", COMMAND));
+        assertEquals("devant|dev", m.getCommand("French", COMMAND));
+        assertEquals("vpered|vr", m.getCommand("Russian", COMMAND));
+    }
+}
