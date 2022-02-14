b0cf14d4495b5d05945d400c52ef03f6bf5cb8b1
Eric Xie <eric.xie@duke.edu>
2022-02-13 23:50:59 -0500

    Finished my use cases

---
+++ doc/plan/APIs/View/TurtleView.java
@@ -0,0 +1,24 @@
+package slogo;
+
+public interface TurtleView{
+
+  // moves the Turtle
+  public default void stepTurtle(int xCoord, int yCoord){
+
+
+
+  }
+
+  // creates animation with a certain speed
+  public default void setUpAnimation(double speed){
+
+
+
+  }
+
+
+
+
+
+
+}
