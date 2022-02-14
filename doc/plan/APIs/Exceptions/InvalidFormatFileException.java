96f62459b2729ebcbd354f19841f8a56085a5e1a
Saad Lahrichi <sl636@duke.edu>
2022-02-14 02:04:33 -0500

    ADD: methods to Exceptions Interfaces

--- doc/plan/APIs/Exceptions/InvalidFormatFileException.java
+++ doc/plan/APIs/Exceptions/InvalidFormatFileException.java
@@ -1,9 +1,14 @@
-package slogo;
+package Exceptions;
 
+/**
+ * this interface checks if the file is a .slogo file
+ */
 public interface InvalidFormatFileException{
 
-
-
-
+  /** the method throws an excpetion if the passed file's
+   * extension is not supported (in this case: .slogo)
+   * @param file passed by the user
+   */
+  public checkFileFormat(File file)
 
 }
