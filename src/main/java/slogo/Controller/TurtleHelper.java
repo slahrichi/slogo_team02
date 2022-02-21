f4201c74ea2d0f7063ec2bb3652c109ab46f182c
Mike Keohane <mbk28@duke.edu>
2022-02-20 23:41:49 -0500

    working on integrating all of the parts

--- src/main/java/slogo/Controller/TurtleHelper.java
+++ src/main/java/slogo/Controller/TurtleHelper.java
@@ -1,18 +1,35 @@
 package slogo.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import slogo.Model.Turtle;
 
 public class TurtleHelper {
   // Can make into list if we have multiple turtles
-  Turtle turtleOne;
+  List<Turtle> turtles;
   List<Command> commands;
+  Turtle currentTurtle;
   public TurtleHelper(){
-    turtleOne = new Turtle(0, 0, commands, 0);
+    Turtle turtleOne = new Turtle(0, 0, 0);
+    turtles = new ArrayList<>();
+    turtles.add(turtleOne);
+    currentTurtle = turtleOne;
+    commands = new ArrayList<>();
+  }
+
+  public void setCurrentTurtle(Turtle currentTurtle) {
+    this.currentTurtle = currentTurtle;
   }
 
   public Turtle getCurrentTurtle(){
-    return turtleOne;
+    return currentTurtle;
   }
 
+  public getCommands(){
+    commands.add(inputParser.getCommands());
+  }
+
+  public step(){
+    currentTurtle.execute(commands[commandIndex]);
+  }
 }
