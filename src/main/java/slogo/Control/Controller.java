84107ddc3979bd24564f43a23ed497f5d29ea5b1
Mike Keohane <mbk28@duke.edu>
2022-02-23 11:10:55 -0500

    added turtle gui call to the Controller. Everything is close to working together

--- src/main/java/slogo/Control/Controller.java
+++ src/main/java/slogo/Control/Controller.java
@@ -2,11 +2,13 @@ package slogo.Control;
 
 import slogo.Model.ModelExceptions;
 import slogo.View.AnimationHandler;
+import slogo.View.TurtleGUI;
 
 public class Controller {
   private InputParser parser;
   private TurtleManager manager;
   private AnimationHandler animationControl;
+  private TurtleGUI turtleGUI;
   public Controller(){
     parser = new InputParser();
     manager = new TurtleManager();
@@ -16,5 +18,6 @@ public class Controller {
   //Need to add ability to pull text and only call step when running a set of commands
   private void updateStep() throws ModelExceptions {
     manager.step();
+   // turtleGUI.updatePosition(manager.getCurrentTurtle());
   }
 }
