fbe68abf1e85db15711cf5a9593a235e0982479e
Mike Keohane <mbk28@duke.edu>
2022-02-25 00:05:56 -0500

    hard coding imputs becauser parser isn't working

--- src/main/java/slogo/Control/Controller.java
+++ src/main/java/slogo/Control/Controller.java
@@ -1,7 +1,12 @@
 package slogo.Control;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import slogo.Model.ModelExceptions;
+import slogo.Model.commands.BackwardCommand;
+import slogo.Model.commands.Command;
+import slogo.Model.commands.ForwardCommand;
 import slogo.View.AnimationHandler;
 import slogo.View.Input.TurtleView;
 
@@ -27,11 +32,18 @@ public class Controller {
     return animationControl;
   }
 
-  public void parseAndSetCommands(String contents)
-      throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-    parser.reset();
-    parser.parseText(contents);
-    manager.setCommands(parser.getCommands());
+  public void parseAndSetCommands() {
+    //parser.reset();
+    //parser.parseText(contents);
+    List<Command> tempCommandList = new ArrayList<>();
+    double[] fifty = {50};
+    double[] hundo = {100};
+    tempCommandList.add(new ForwardCommand(fifty));
+    tempCommandList.add(new BackwardCommand(fifty));
+    tempCommandList.add(new ForwardCommand(fifty));
+    tempCommandList.add(new ForwardCommand(fifty));
+    tempCommandList.add(new BackwardCommand(hundo));
+    manager.setCommands(tempCommandList);
   }
 
   //Need to add ability to pull text and only call step when running a set of commands
