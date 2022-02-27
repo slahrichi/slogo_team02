e5cd0b466251e806805656d584c849f967336a50
Haris Adnan <haris.adnan@duke.edu>
2022-02-27 18:28:50 -0500

    added Boolean Opertation abstract class

---
+++ src/main/java/slogo/Model/Commands/Boolean/BooleanOperation.java
@@ -0,0 +1,10 @@
+package slogo.Model.Commands.Boolean;
+
+import slogo.Model.Commands.Command;
+
+public abstract class BooleanOperation extends Command {
+  
+  public BooleanOperation(double[] values) {
+    super(values);
+  }
+}
