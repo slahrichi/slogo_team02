e3c148d3d4c30d83f7e1b336f09a4ac46b03f599
Haris Adnan <haris.adnan@duke.edu>
2022-02-26 23:20:47 -0500

    added Variable Manager Class with excoetions

---
+++ src/main/java/slogo/Control/ControllerExceptions/VariableExceptions.java
@@ -0,0 +1,10 @@
+package slogo.Control.ControllerExceptions;
+
+public class VariableExceptions extends Exception{
+  public ExceptionVariableAlreadyInUse(String message){
+    super(message);
+  }
+
+}
+
+
