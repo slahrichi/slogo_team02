48f6f844b89b0949d612a188cd09cabb56a5d45b
Haris Adnan <haris.adnan@duke.edu>
2022-02-28 01:28:00 -0500

    added comments for some model classes

--- src/main/java/slogo/Model/Variable.java
+++ src/main/java/slogo/Model/Variable.java
@@ -9,6 +9,11 @@ public class Variable {
   private SimpleStringProperty name;
   private SimpleDoubleProperty value;
 
+  /**
+   * Class that defines a variable
+   * @param name : name of a variable
+   * @param value : value to be assigned to a variable
+   */
   public Variable(String name, double value) {
     this.name = new SimpleStringProperty(name);
     this.value = new SimpleDoubleProperty(value);
@@ -29,51 +34,5 @@ public class Variable {
     this.value.set(Double.parseDouble(string));
   }
 
-  public static class VariableManager {
-    private Map<String, Double> variableMap;
-    private List<Variable> variableList ;
 
-    public VariableManager(){
-
-    }
-    public void makeVariable(String name, String value) throws VariableExceptions {
-      if (!variableMap.containsKey(name)){
-        variableMap.put(name, Double.parseDouble(value));
-        Variable newVar = new Variable(name, Double.parseDouble(value));
-        variableList.add(newVar);
-      }
-      else{
-        throw new VariableExceptions("This variable name already exists. Use the Set expression instead.");
-      }
-    }
-    public void setVariable(String name, String value) throws VariableExceptions {
-      Variable thisVariable = null;
-      if (variableMap.containsKey(name)){
-        for (Variable variable: variableList){
-          if(variable.getName().equals(name)){
-            thisVariable = variable;
-          }
-        }
-        // update List
-        variableList.remove(thisVariable);
-        thisVariable.setValue(value);
-        variableList.add(thisVariable);
-        //update Map
-        variableMap.put(name, Double.valueOf(value));
-      }
-
-      else{
-        throw new VariableExceptions("This variable does not exist");
-      }
-    }
-    public double getVariableValue(String variableName){
-      double variableVal = Double.parseDouble(String.valueOf(variableMap.get(variableName)));
-      return variableVal;
-    }
-    public Variable makeVariableForNoValue(String variableNameUsedByUser){
-      Variable variable = new Variable(variableNameUsedByUser, 0);
-      return variable;
-    }
-
-  }
 }
