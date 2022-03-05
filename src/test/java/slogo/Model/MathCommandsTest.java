32640eeee0b3465876010f4647da055913bc6302
Haris Adnan <haris.adnan@duke.edu>
2022-03-04 23:24:12 -0500

    added Math Command Tests

--- src/test/java/slogo/Model/MathCommandsTest.java
+++ src/test/java/slogo/Model/MathCommandsTest.java
@@ -6,6 +6,18 @@ import org.junit.jupiter.api.Test;
 import slogo.Model.Commands.Command;
 import slogo.Model.Commands.MathCommands.ArcTangentCommand;
 import slogo.Model.Commands.MathCommands.CosineCommand;
+import slogo.Model.Commands.MathCommands.DifferenceCommand;
+import slogo.Model.Commands.MathCommands.MinusCommand;
+import slogo.Model.Commands.MathCommands.NaturalLogCommand;
+import slogo.Model.Commands.MathCommands.PiCommand;
+import slogo.Model.Commands.MathCommands.PowerCommand;
+import slogo.Model.Commands.MathCommands.ProductCommand;
+import slogo.Model.Commands.MathCommands.QuotientCommand;
+import slogo.Model.Commands.MathCommands.RemainderCommand;
+import slogo.Model.Commands.MathCommands.SineCommand;
+import slogo.Model.Commands.MathCommands.SquareRootCommand;
+import slogo.Model.Commands.MathCommands.SumCommand;
+import slogo.Model.Commands.MathCommands.TangentCommand;
 
 public class MathCommandsTest {
   TurtleManager turtleManager = new TurtleManager();
@@ -29,6 +41,92 @@ public class MathCommandsTest {
     CosineCommand cosineCommand = new CosineCommand(input);
     assertEquals(cosineCommand.getValue(), Math.cos(Math.toRadians(input[0])));
   }
+
+  @Test
+  void DifferenceCommand(){
+    setupTest(new double[] {5, 3});
+    DifferenceCommand differenceCommand = new DifferenceCommand(input);
+    assertEquals(differenceCommand.getValue(), 2);
+  }
+  @Test
+  void MinusCommand(){
+    setupTest(new double[] {5});
+    MinusCommand minusCommand = new MinusCommand(input);
+    assertEquals(-1 * 5, minusCommand.getValue());
+
+  }
+  @Test
+  void NaturalLogCommand(){
+    setupTest(new double[] {4});
+    NaturalLogCommand naturalCommand = new NaturalLogCommand(input);
+    assertEquals(naturalCommand.getValue(), Math.log(4));
+  }
+  @Test
+  void PiCommandTest(){
+    setupTest(new double[] {2});
+    PiCommand picommand = new PiCommand(input);
+    assertEquals(Math.PI, picommand.getValue());
+  }
+  @Test
+  void PowerCommandTest(){
+    setupTest(new double[] {5, 3});
+    PowerCommand powerCommand = new PowerCommand(input);
+    assertEquals(powerCommand.getValue(), Math.pow(5, 3));
+  }
+
+  @Test
+  void ProductCommand(){
+    setupTest(new double[]{4, 6});
+    ProductCommand productCommand = new ProductCommand(input);
+    assertEquals(productCommand.getValue(), 24);
+  }
+  @Test
+  void QuotientCommand(){
+    setupTest(new double[]{10, 5});
+    QuotientCommand quotientCommand = new QuotientCommand(input);
+    assertEquals(2, quotientCommand.getValue());
+  }
+  @Test
+  void RemainderCommand(){
+    setupTest(new double[]{10 , 8});
+    RemainderCommand remainderCommand = new RemainderCommand(input);
+    assertEquals(2, remainderCommand.getValue());
+  }
+
+  @Test
+  void SineCommands(){
+    setupTest(new double[]{30.0});
+    SineCommand sineCommand = new SineCommand(input);
+    assertEquals(Math.sin(Math.toRadians(30.0)), sineCommand.getValue());
+
+  }
+  @Test
+  void SquareRootCommand(){
+    setupTest(new double[]{4});
+    SquareRootCommand squareRootCommand = new SquareRootCommand(input);
+    assertEquals(2, squareRootCommand.getValue());
+  }
+  @Test
+  void SumCommand(){
+    setupTest(new double[]{4, 5});
+    SumCommand sumCommand = new SumCommand(input);
+    assertEquals(9, sumCommand.getValue());
+  }
+  @Test
+  void TangentCommand(){
+    setupTest(new double[]{32.0});
+    TangentCommand tangentCommand = new TangentCommand(input);
+    assertEquals(Math.tan(Math.toRadians(32.0)), tangentCommand.getValue());
+
+  }
+
+
+
+
+
+
+
+
 
 
 
