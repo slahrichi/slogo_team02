9484f26a5bc055a66b9ee044892f30772fe54e17
Mike Keohane <mbk28@duke.edu>
2022-03-21 00:56:25 -0400

    Added a couple comments for my masterpiece. Just wanted to show off the classes I worked on the most even though they are kind of lame

--- src/main/java/slogo/Control/ControllerAPI.java
+++ src/main/java/slogo/Control/ControllerAPI.java
@@ -3,7 +3,12 @@ package slogo.Control;
 import java.util.List;
 import slogo.Model.TurtleManagerException;
 import slogo.Model.TurtleRecord;
-
+/**
+ * @author Mike Keohane
+ *
+ * The controller parses and runs an input of strings on the turtles
+ * It also contains and updates the history
+ */
 public interface ControllerAPI {
 
   void parseAndRunCommands(String contents) throws Exception;
