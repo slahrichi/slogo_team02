65a47c791f0a8e049622854275782f32916e22cb
Haris Adnan <haris.adnan@duke.edu>
2022-03-21 23:38:04 -0400

    finished with mastercode

--- src/main/java/slogo/MasterPiece/TurtleManagerException.java
+++ src/main/java/slogo/MasterPiece/TurtleManagerException.java
@@ -4,6 +4,10 @@ package slogo.MasterPiece;
  * Exception class that handles if the wrong / unavailable turtles are accessed. Is thrown with move
  * commands and Queries and in methods in TurtleManager class
  *
+ * Shows that excpetions are handled for when the turtle being tried to accessed is not existent /
+ * to handle any other TurtleManager Exceptions
+ *
+ * git commits: https://coursework.cs.duke.edu/compsci308_2022spring/slogo_team02/-/commit/cd25911161378afb69541ba40801e9084791a3ae
  * @author Haris Adnan
  */
 public class TurtleManagerException extends Exception {
@@ -18,9 +22,4 @@ public class TurtleManagerException extends Exception {
     super(message);
 
   }
-
-  public TurtleManagerException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
 }
